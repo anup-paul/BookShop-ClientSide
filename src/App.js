e70c6fc60884bf334551cf798f8c7fdaf7bacc28
anup-paul <anuppaul1845@gmail.com>
2021-03-30 23:00:39 +0600

    starting-project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello dude</h1>
+      <button className="btn btn-primary">click me</button>
+    </div>
+  );
+}
+
+export default App;
